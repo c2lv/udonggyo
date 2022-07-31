51937d8a332636e1e9cb730aa43f0510b9a856d2
c2lv <hyeonjun7@naver.com>
2022-07-31 16:53:45 +0900

    first commit

---
+++ app/src/main/java/com/example/ourvillageedu/ui/home/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.example.ourvillageedu.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.ourvillageedu.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
